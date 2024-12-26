3555c3983c6be6a8e614568155ff440a02ca5ed7
RafayMemon01 <rafayrashid200611@gmail.com>
2024-12-26 16:59:06 +0500

    new font playfair added class custom tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
-  extend: { backgroundImage: {
-    'hero-bg': "url('./assets/bg-home-main.png')",
-  }},
+  extend: {
+    backgroundImage: {
+      'hero-bg': "url('./assets/bg-home-main.png')",
+    },
+    fontFamily: {
+      'playfair': ['Playfair Display', 'serif'],
+    },
+  },
 };
 export const plugins = [];
 

59c4435150b8eed54ecafaf449eef45e58d2939d
RafayMemon01 <rafayrashid200611@gmail.com>
2024-12-26 16:41:55 +0500

    small changes on header and first section header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  extend: { backgroundImage: {
+    'hero-bg': "url('./assets/bg-home-main.png')",
+  }},
 };
 export const plugins = [];
 
